9fe793bc37a1414bef00d0df0b3d6036aa854945
Rodrigo N <rodrigo.gomes65@etec.sp.gov.br>
2020-10-14 11:40:39 -0300

    Tres paginas ja foram criadas antes desse primeiro commit

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,39 @@
+//cria o mapa
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+const map = L.map('mapid', options).setView([-23.7108781,-46.7940031], 15);
+
+//cria a camada 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',).addTo(map);
+
+//icones map    
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    popupAnchor:[170, 2]
+})
+
+
+
+//markers daora    
+L.marker([-23.7108781,-46.7940031],{icon:icon}).addTo(map)
+    
+function selectImage(event){
+    const button = event.currentTarget;
+    const buttons = document.querySelectorAll(".images button");
+    buttons.forEach(removerActiveClass);
+
+    function removerActiveClass(button){
+        button.classList.remove("active");
+    }
+    button.classList.add('active')
+    const image = button.children[0];
+    const imageContainer = document.querySelector(".orphanage-details > img");
+    imageContainer.src = image.src;
+}
---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,30 @@
+//cria o mapa
+const map = L.map('mapid').setView([-23.7108781,-46.7940031], 15);
+
+//cria a camada 
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+    // {
+    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }
+    ).addTo(map);
+
+//icones map    
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    popupAnchor:[170, 2]
+})
+
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Orfanato exemplo <a href="orphanage.html?id=10" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+
+//markers daora    
+L.marker([-23.643427788201457, -46.68510700351251],{icon:icon}).addTo(map)
+    .bindPopup(popup)
+    .openPopup();
