28285939ccbb5c46bc489a991f2c6eac6bd73eb1
Rodrigo N <rodrigo.gomes65@etec.sp.gov.br>
2020-10-14 22:02:25 -0300

    todas as funcionalidades da criar orfanato criadas

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -14,17 +14,76 @@ const icon = L.icon({
     iconUrl: "./public/images/map-marker.svg",
     iconSize: [58,68],
     iconAnchor: [29, 68],
-    popupAnchor:[170, 2]
 })
 
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Orfanato exemplo <a href="./orphanage.html" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
-                                        //orphanage.html?id=10   
+let marker;
+ 
 //markers daora    
-L.marker([-23.643427788201457, -46.68510700351251],{icon:icon}).addTo(map)
-    .bindPopup(popup)
-    .openPopup();
+map.on('click', (event) =>{
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+    //insere os valores no input
+    document.querySelector("[name=latitude]").value = lat;
+    document.querySelector("[name=longitude]").value = lng;
+
+    //tira os markers anteriores
+    marker && map.removeLayer(marker);
+
+    //colocar icons daora
+    marker = L.marker([lat,lng], {icon}).addTo(map);
+});
+
+//upar fotos 
+function addFoto(){
+    //puxar o container de fotos #imgs
+    const container = document.querySelector("#imgs");
+    //pegar o container a se duplicar .new-upload
+    const fieldsContainer = document.querySelectorAll(".new-upload");
+    //clonar a img upada
+    const novoContainer = fieldsContainer[fieldsContainer.length-1].cloneNode(true);
+    //verifica se ta vazio
+    const input = novoContainer.children[0];
+    if(input.value == ""){
+       return;
+    }
+    //limpa o campo de texto
+    novoContainer.children[0].value = "";
+
+    //botar o clone no container #imgs
+    container.appendChild(novoContainer);
+}
+
+function deletarFoto(event){
+    console.log(event.currentTarget);
+    const span = event.currentTarget
+    //verifica se tem fotos upadas
+    const fieldsContainer = document.querySelectorAll(".new-upload");
+    if(fieldsContainer.length < 2){
+        //limpa o valor
+        span.parentNode.children[0].value = "";
+        return
+    };
+    //remove o campo
+    span.parentNode.remove();
+}
+
+//alternar sim e não
+function alternarAtivo(event){
+    //pegar o botão clicado
+    const button = event.currentTarget
+    
+    
+    //trocar o hidden
+    const input = document.querySelector('[name="abre-fds"]');
+    //verificar o botão que foi clicado
+    input.value = button.dataset.value;
+    //console.log(input.value)
+    //tirar a .active
+    document.querySelectorAll(".button-select button")
+    .forEach((button) => {
+        button.classList.remove('active');
+    })
+    //colocar a .active 
+    button.classList.add('active');
+    
+}
